bc5d074cdaab491aa499635dd80e96976dfd2240
Artur Kuzmich <Artur1Kuzmich@gmail.com>
2020-08-21 17:03:36 +0300

    adaptive carCard to 1024px

--- resources/views/site/auto/card/ru.blade.php
+++ resources/views/site/auto/card/ru.blade.php
@@ -23,4 +23,237 @@
         </div>
         <ycardauto></ycardauto>
     </section>
+    <section class="yb-options specifications ">
+            <div class="yb-technical_info">
+                <div class="yb-vis_info">
+                    <div class="y-info_row">
+                        <div class="y-info_title vis_l">
+                            <span>Габариты и масса</span>
+                        </div>
+                        <div class="y-info_title vis_r">
+                            <span>Двигатель</span>
+                        </div>
+                    </div>
+                    <div class="y-info_column">
+                        <div class="y-items_info vis_l">
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Длина, мм</span>
+                                <span class="y_item">4939</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Ширина, мм</span>
+                                <span class="y_item">2010</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Объем багажника, л</span>
+                                <span class="y_item">1790</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Колесная база, мм</span>
+                                <span class="y_item">2935</span>
+                            </div>
+                        </div>
+                        <div class="y-items_info vis_r">
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Расположение</span>
+                                <span class="y_item">Спереди(продольно)</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Топливо</span>
+                                <span class="y_item">Бензин</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Объем, см3</span>
+                                <span class="y_item">299</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Количество цилиндров</span>
+                                <span class="y_item">6</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Расположение цилиндров</span>
+                                <span class="y_item">Рядное</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Наличие дополнительного давления</span>
+                                <span class="y_item">Турбокомпрессор</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="yb-vis_info">
+                    <div class="y-info_row">
+                        <div class="y-info_title vis_l">
+                            <span>Кузов</span>
+                        </div>
+                        <div class="y-info_title vis_r">
+                            <span>Трансмиссия</span>
+                        </div>
+                    </div>
+                    <div class="y-info_column">
+                        <div class="y-items_info vis_l">
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Количество дверей</span>
+                                <span class="y_item">4</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Тип кузова</span>
+                                <span class="y_item">Кроссовер</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Количество мест</span>
+                                <span class="y_item">5</span>
+                            </div>
+                        </div>
+                        <div class="y-items_info vis_r">
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Коробка передач</span>
+                                <span class="y_item">Автомат</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Количество передач</span>
+                                <span class="y_item">6</span>
+                            </div>
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Тип привода</span>
+                                <span class="y_item">Полный</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="yb-vis_info">
+                    <div class="y-info_row">
+                        <div class="y-info_title vis_l">
+                            <span>Рулевое управление</span>
+                        </div>
+                        <div class="y-info_title vis_r">
+                            <span>Мультимедия</span>
+                        </div>
+                    </div>
+                    <div class="y-info_column">
+                        <div class="y-items_info vis_l">
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Усилитель руля</span>
+                                <span class="y_item">Электроусилитель</span>
+                            </div>
+                        </div>
+                        <div class="y-items_info vis_r">
+                            <div class="yvis_items_column">
+                                <span class="y_item-label">Дисплей, дюйм</span>
+                                <span class="y_item">Есть</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+    </section>
+    <h3>Опции комплектации</h3>
+    <section class="yb-options configuration ">
+        <div class="yb-technical_info">
+            <div class="yb-vis_info">
+                <div class="y-info_row">
+                    <div class="y-info_title vis_l">
+                        <span>Интерьер</span>
+                    </div>
+                    <div class="y-info_title vis_r">
+                        <span>Экстерьер</span>
+                    </div>
+                </div>
+                <div class="y-info_column">
+                    <div class="y-items_info vis_l">
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Отделка салона</span>
+                            <span class="y_item">Пластик, Кожа, Отделка Alcantara </span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Обшивка сидений</span>
+                            <span class="y_item">Комбинированна с перфорацией кожа</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Оплетка руля</span>
+                            <span class="y_item">Кожаная</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Ковры</span>
+                            <span class="y_item">Салона</span>
+                        </div>
+                    </div>
+                    <div class="y-items_info vis_r">
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Диски</span>
+                            <span class="y_item">Легкосплавные</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Фары</span>
+                            <span class="y_item">Адаптивные, Противотуманные, Светодиодные , Задние противотуманные, Дневные ходовые огни</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Зеркала</span>
+                            <span class="y_item">Автозатемнение, Память, Повторители, Складывающиеся</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Люк</span>
+                            <span class="y_item">Есть</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Обтекатели</span>
+                            <span class="y_item">Верхний обтекатель, Бампер</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="yb-vis_info">
+                <div class="y-info_row">
+                    <div class="y-info_title vis_l">
+                        <span>Комфорт</span>
+                    </div>
+                    <div class="y-info_title vis_r">
+                        <span>Безопасность</span>
+                    </div>
+                </div>
+                <div class="y-info_column">
+                    <div class="y-items_info vis_l">
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Климатконтроль</span>
+                            <span class="y_item">Четырехзонный</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Электрооборудование</span>
+                            <span class="y_item">Датчик давления в шинах, Электропривод рулевой колонки, Датчик света, Подогрев руля, Цифровая панель приборов, Омыватель фар, Электропривод багажника, Память рулевой колонки, Обогрев дворников, Подогрев лобового стекла, Камера кругового обзора, Бортовой компьютер, Корректор фар, Подогрев заднего стекла, Круиз-контроль, Hands Free, Режимы движения, Парктроник, Navi, Кнопка запуска двигателя, Электропривод сидений, Start/Stop, Память сидений</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Подогрев сидений</span>
+                            <span class="y_item">С подогревом</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Электрические зеркала</span>
+                            <span class="y_item">Передние, Задние</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Электростеклоподъемники</span>
+                            <span class="y_item">Передние, Задние</span>
+                        </div>
+                    </div>
+                    <div class="y-items_info vis_r">
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Замки</span>
+                            <span class="y_item">Центральный замок</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Airbag (подушки безопасности)</span>
+                            <span class="y_item">Водителя, Пассажира, Боковые</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Системы безопасности</span>
+                            <span class="y_item">BAS, Система BrakeAssist, VSW, TRC, ACC, AHS, DAC, AHB, EPB, HAC, EBS, DRS, HSA, TPWS, BSM, TSR, ПБС, LDA, EBD, ABS, LKA</span>
+                        </div>
+                        <div class="yvis_items_column">
+                            <span class="y_item-label">Система курсовой устойчивости</span>
+                            <span class="y_item">ESP</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </main>
