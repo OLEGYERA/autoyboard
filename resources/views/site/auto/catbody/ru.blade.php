b9b9ffd23a8dd92cb774dc7f6925ed77a11a6cca
Your Name <artur1kuzmich@gmail.com>
2020-10-19 18:11:36 +0300

    finish changeLog, catBody adaptive / convention DropDown create add func jQuery to show and selected item

---
+++ resources/views/site/auto/catbody/ru.blade.php
@@ -0,0 +1,132 @@
+<main class="yb-content">
+    <section class="yb-body_vis">
+        <div class="yb-body_crumbs">
+             <span>
+                <a href="#">Главная</a>
+                <i class="fas fa-angle-right"></i>
+            </span>
+            <span>
+                <a href="#">Каталог </a>
+                <i class="fas fa-angle-right"></i>
+            </span>
+            <span>
+                <a class="active_crumb" href="#"> Кузовы</a>
+            </span>
+        </div>
+        <h1>Кузовы мотоциклов</h1>
+{{--        <div class="wrapper">--}}
+{{--            <input type="text" placeholder="Выберите кузов" class="show-list">--}}
+{{--            <div class="check-list">--}}
+{{--                <ul>--}}
+{{--                    <li>--}}
+{{--                        <input type="checkbox" value="Легковые" class="check-list__checkbox" />Легковые</li>--}}
+{{--                    <li>--}}
+{{--                        <input type="checkbox" value="Мото" class="check-list__checkbox" />Мото</li>--}}
+{{--                    <li>--}}
+{{--                        <input type="checkbox" value="Прицепы" class="check-list__checkbox" />Прицепы</li>--}}
+{{--                </ul>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+        <div class="yb_selected-body">
+            <div class="selected_body">
+                <span class="yb-body_choice">Выберите кузов</span>
+                <i class="fa fa-chevron-down"></i>
+            </div>
+            <div class="yb-body_list">
+                <div class="yb-body_item ">
+                    <figure class="yb_body_img">
+                        <img src="{{asset('img/system/static/passenger_car.png')}}" alt="">
+                    </figure>
+                    <span>Легковые</span>
+                    <i  class="fas fa-check"></i>
+                </div>
+                <div class="yb-body_item ">
+                    <figure class="yb_body_img">
+                        <img src="{{asset('img/system/static/moto.png')}}" alt="">
+                    </figure>
+                    <span>Мото</span>
+                    <i class="fas fa-check"></i>
+                </div>
+                <div class="yb-body_item">
+                    <figure class="yb_body_img">
+                        <img src="{{asset('img/system/static/trailers.png')}}" alt="">
+                    </figure>
+                    <span>Прицепы</span>
+                    <i class="fas fa-check"></i>
+                </div>
+                <div class="yb-body_item ">
+                    <figure class="yb_body_img">
+                        <img src="{{asset('img/system/static/spec_equipment.png')}}" alt="">
+                    </figure>
+                    <span>Спецтехника</span>
+                    <i class="fas fa-check"></i>
+                </div>
+                <div class="yb-body_item ">
+                    <figure class="yb_body_img">
+                        <img src="{{asset('img/system/static/agro_machinery.png')}}" alt="">
+                    </figure>
+                    <span>Сельхозтехника</span>
+                    <i class="fas fa-check"></i>
+                </div>
+                <div class="yb-body_item ">
+                    <figure class="yb_body_img">
+                        <img src="{{asset('img/system/static/buses.png')}}" alt="">
+                    </figure>
+                    <span>Автобусы</span>
+                    <i class="fas fa-check"></i>
+                </div>
+                <div class="yb-body_item ">
+                    <figure class="yb_body_img">
+                        <img src="{{asset('img/system/static/water.png')}}" alt="">
+                    </figure>
+                    <span>Водный</span>
+                    <i class="fas fa-check"></i>
+                </div>
+            </div>
+        </div>
+        <div class="yb-changetype_items">
+            <button class="yb-type_item active_tab">Все </button>
+            <button class="yb-type_item">Популярные</button>
+            <button class="yb-type_item">Мотоциклы</button>
+            <button class="yb-type_item">Квадроциклы</button>
+            <button class="yb-type_item">Мини мотоциклы</button>
+            <button class="yb-type_item">Мопеды</button>
+        </div>
+        <div class="yb-type_list">
+            <ul class="yb-type_items">
+                <li class="yb-type_item">Скутер / Мотороллер</li>
+                <li class="yb-type_item">Макси-скутер</li>
+                <li class="yb-type_item">Мотоцикл Туризм</li>
+                <li class="yb-type_item">Квадроцикл спортивный</li>
+                <li class="yb-type_item">Мотоцикл Спорт-туризм</li>
+                <li class="yb-type_item">Мотоцикл Многоцелевой (All-round)</li>
+                <li class="yb-type_item">Мини спорт</li>
+                <li class="yb-type_item">Мини крос (Питбайк)</li>
+                <li class="yb-type_item">Квадроцикл утилитарный</li>
+                <li class="yb-type_item">Квадроциклы</li>
+                <li class="yb-type_item">Мопеды</li>
+                <li class="yb-type_item">Мотоцикл Внедорожный</li>
+                <li class="yb-type_item">Мотоцикл Супермото</li>
+                <li class="yb-type_item">Трайк</li>
+                <li class="yb-type_item">Мотоциклы</li>
+                <li class="yb-type_item">Спортбайк</li>
+                <li class="yb-type_item">Вездеход-амфибия</li>
+                <li class="yb-type_item">Мотоцикл Кросс</li>
+                <li class="yb-type_item">Трицикл</li>
+                <li class="yb-type_item">Мотовездеход</li>
+                <li class="yb-type_item">Картинг</li>
+                <li class="yb-type_item">Мотоцикл Классик</li>
+                <li class="yb-type_item">Снегоход</li>
+                <li class="yb-type_item">Мотоцикл Без обтекателей (Naked bike)</li>
+                <li class="yb-type_item">Мотоцикл Триал</li>
+                <li class="yb-type_item">Мотоцикл Чоппер</li>
+                <li class="yb-type_item">Мотоцикл Круизер</li>
+                <li class="yb-type_item">Мотоцикл с коляской</li>
+                <li class="yb-type_item">Мотоцикл Кастом</li>
+                <li class="yb-type_item">Мини мотоциклы</li>
+                <li class="yb-type_item">Квадроцикл детский</li>
+                <li class="yb-type_item">Гольф-кар</li>
+            </ul>
+        </div>
+    </section>
+</main>
