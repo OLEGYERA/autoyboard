23eb87c6af1eea9c5346955184844478444ef85b
Artur <Artur1Kuzmich@gmail.com>
2020-11-03 18:28:08 +0200

    start visualization info in card page

--- resources/js/site/site.js
+++ resources/js/site/site.js
@@ -12,22 +12,22 @@ Vue.component('ymenu', require('./modules/header/ymenu.vue').default);
 // Vue.component('ymobileanalitics', require('./components/ymobileanalitics').default);
 // //finish mobile
 // //change visible
-// Vue.component('ychangeoffers', require('./components/ybestofferschange').default);
+Vue.component('ychangeoffers', require('./components/ybestofferschange').default);
 // Vue.component('ynewschange', require('./components/ynewschange').default);
 // //finish change visible
 //
 Vue.component('ysearch', require('./modules/header/ysearch.vue').default);
 Vue.component('ylocation', require('./components/ylocation.vue').default);
 // Vue.component('ystatus', require('./components/ystatus').default);
-// Vue.component('yexpanded', require('./components/yexpanded').default);
+Vue.component('yexpanded', require('./components/yexpanded').default);
 // Vue.component('yform', require('./components/yform').default);
 Vue.component('ycheckbox', require('./components/ycheckbox').default);
 Vue.component('yratio', require('./components/yratio').default);
 
-// Vue.component('ytitle', require('./components/ytitle').default);
-// Vue.component('ybestoffer', require('./components/ybestoffers').default);
+Vue.component('ytitle', require('./components/ytitle').default);
+Vue.component('ybestoffer', require('./components/ybestoffers').default);
 // Vue.component('ycatalog', require('./components/ycatalog').default);
-// Vue.component('yfavorite', require('./components/yfavorite').default);
+Vue.component('yfavorite', require('./components/yfavorite').default);
 // Vue.component('yanalitics', require('./components/yanalitics').default);
 // Vue.component('ynews', require('./components/ynews').default);
 // Vue.component('yfaqquestions', require('./components/yfaqquestions').default);
@@ -55,7 +55,7 @@ Vue.component('ychaccordion', require('./components/ychaccordion').default);
 //
 // Vue.component('ystickers', require('./components/ystickers').default);
 //
-// Vue.component('ycardauto', require('./components/ycardauto').default);
+Vue.component('ycardauto', require('./components/ycardauto').default);
 //
 
 
